cef617042c9ac207739de5d2aa3c41cb42d440d6
rhea-n-t <rhea.nt@surya-digital.com>
2024-08-23 16:42:36 +0530

    Move screenshot utils to a common file

--- tests/HomePageVisual.spec.js
+++ tests/HomePageVisual.spec.js
@@ -1,22 +1,7 @@
 // Visual Testing
 import { test } from "@playwright/test";
 import { Loginpage } from "../pages/Loginpage";
-
-const setViewPort = async (page, querySelector)=>{
-  const currentViewport= page.viewportSize()
-  const pixelsNotInView = await page.evaluate(({ querySelector, currentViewport })=>{
-    const content = document.querySelector(querySelector)
-    return  content.scrollHeight - content.clientHeight+ currentViewport.height
-  }, { querySelector, currentViewport })
-  await page.setViewportSize({
-    width: currentViewport.width,
-    height:  pixelsNotInView
-  })
-}
-
-const resetViewPort = async (page)=>{
-  await page.setViewportSize({ width: 1536, height: 730 })
-}
+import { resetViewPort, setViewPort } from "./utils/viewPortScreenShotUtils";
 
 test("Example Test", async ({ page }) => {
   const login = new Loginpage(page);
